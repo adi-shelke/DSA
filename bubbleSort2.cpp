b9902b6f648427608f9497e000a82cd34944d4cf
Adinath Shelke <shelkeadinath3@gmail.com>
2023-01-18 18:29:36 +0530

    day 4 commit 2

---
+++ bubbleSort2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int size;
+    cin>>size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+        cin>>arr[i];
+    
+    for (int i = 1; i < size; i++)
+    {
+        for(int j=0;j<size-i;j++)
+        {
+            if(arr[j]>arr[j+1])
+                swap(arr[j],arr[j+1]);
+        }
+    }
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
