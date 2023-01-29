0f9eab11c858bf7274353098b9382d10fbaed65c
Adinath Shelke <shelkeadinath3@gmail.com>
2023-01-30 00:20:40 +0530

    BST operations

---
+++ BST.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data){
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+};
+class BST{
+    public:
+    Node* insert(int data,Node* root)
+    {
+        if(root==NULL)
+            root=new Node(data);
+        else
+        {
+            Node* temp=root;
+            if(data<temp->data)
+                root->left=insert(data,temp->left);
+            else
+                root->right=insert(data,temp->right);
+        }
+        return root;
+    }
+    void displayInorder(Node* root)
+    {
+        if(root==NULL)
+            return;
+        else
+        {
+            displayInorder(root->left);
+            cout<<root->data<<" ";
+            displayInorder(root->right);
+        }
+    }
+    void displayPreorder(Node* root)
+    {
+        if(root==NULL)
+            return;
+        else
+        {
+            cout<<root->data<<" ";
+            displayPreorder(root->left);
+            displayPreorder(root->right);
+        }
+    }
+    void displayPostorder(Node* root)
+    {
+        if(root==NULL)
+            return;
+        else
+        {
+            displayPostorder(root->left);
+            displayPostorder(root->right);
+            cout<<root->data<<" ";
+        }
+    }
+    bool search(Node* root, int key)
+    {
+        if(root==NULL)
+            return false;
+        else if(root->data==key)
+            return true;
+        else if(root->data<key)
+            search(root->right,key);
+        else
+            search(root->left,key);
+    }
+    
+};
+int main()
+{
+    Node* root=NULL;
+    BST bst;
+    root=bst.insert(10,root);
+    root=bst.insert(50,root);
+    root=bst.insert(70,root);
+    root=bst.insert(20,root);
+    root=bst.insert(30,root);
+    root=bst.insert(40,root);
+    root=bst.insert(5,root);
+    bst.displayInorder(root);
+    cout<<endl;
+    bst.displayPreorder(root);
+    cout<<endl;
+    bst.displayPostorder(root);
+    cout<<endl;
+    cout<<bst.search(root,80);
+}
