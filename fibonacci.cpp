c2615af9a511867b8244e210725846ac65836565
Adinath Shelke <shelkeadinath3@gmail.com>
2023-02-02 22:17:18 +0530

    fibonacci series

---
+++ fibonacci.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+void fib(int num)
+{
+    int first,second;
+    first=0;
+    second=1;
+    for(int i=1;i<=num;i++)
+    {
+        cout<<first<<" ";
+        int temp=first+second;
+        first=second;
+        second=temp;
+    }
+    return;
+    
+}
+int main()
+{
+
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+
+    int num;
+    cin>>num;
+
+    fib(num);
+    return 0;
+}
