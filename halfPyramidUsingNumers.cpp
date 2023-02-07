09b434442ca7644312c962ea9cc55f7118c74644
Adinath Shelke <shelkeadinath3@gmail.com>
2023-02-07 22:16:19 +0530

    half pyramid

---
+++ halfPyramidUsingNumers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+
+    int num;
+    cin>>num;
+
+    for(int i=1;i<=num;i++)
+    {
+        for(int j=1;j<=i;j++)
+            cout<<i<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
