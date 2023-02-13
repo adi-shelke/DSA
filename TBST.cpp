de5282f77cbf206fed7b88ab7d1f10abfd7933b2
Adinath Shelke <shelkeadinath3@gmail.com>
2023-02-13 15:14:27 +0530

    TBST template created

---
+++ TBST.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    bool lbit,rbit;
+
+    Node(int data)
+    {
+        this->data=data;
+        left=right=NULL;
+        rbit=lbit=1;
+    }
+};
+
+class TBST{
+    public:
+    Node* dummy;
+    Node* root;
+    TBST(){
+        dummy=new Node(-1);
+        dummy->rbit=1;
+        dummy->lbit=0;
+        dummy->left=dummy;
+        dummy->right=dummy;
+    }
+    void insert(int data)
+    {
+        if(dummy->lbit=1)
+        {
+            root=new Node(data);
+            root->left=dummy->left;
+            root->lbit=dummy->lbit;
+            root->right=dummy->right;
+            root->rbit=1;
+            root->lbit=0;
+            dummy->left=root;
+        }
+        else
+        {
+            if(data<root->data)
+            {
+                
+            }
+        }
+
+    }
+};
+
+int main()
+{
+    TBST tbst;
+}
