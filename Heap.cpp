f9c4bde27199da76b9fc066d1b811e27988bcbf9
Adinath Shelke <shelkeadinath3@gmail.com>
2023-04-07 18:52:30 +0530

    Heap sort delete function

--- Heap.cpp
+++ Heap.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 #include<cmath>
 using namespace std;
-void insert(vector <int> & arr, int size, int el){
+void insert(vector <int> & arr, int el){
     arr.push_back(el);
     int current,parent; // current to store the index of child and parent to store the index of the parent node.
     current=arr.size()-1;
@@ -16,17 +16,54 @@ void insert(vector <int> & arr, int size, int el){
             return;
     }
 }
+void heapify(vector <int> & arr,int i){
+    int largest = i;
+    int left = 2*i+1;
+    int right = 2*i+2;
+
+    if(left<arr.size()-1 && arr[left]>arr[largest])
+        largest=left;
+    if(right<arr.size()-1 && arr[right]>arr[largest])
+        largest=right;
+    if(largest!=i){
+        swap(arr[i],arr[largest]);
+        heapify(arr,largest);
+    }
+    cout.flush();
+}
+void deletion(vector <int> & arr,vector <int> & sorted){
+    sorted.push_back(arr[0]);
+    swap(arr[0],arr[arr.size()-1]);
+    arr.pop_back();
+    heapify(arr,0);
+}
+void heapSort(vector <int> & copy,vector <int> & sorted){
+    while(copy.size()-1>0)
+        deletion(copy,sorted);
+    cout.flush();
+}
 int main(){
     vector <int> arr; // initializing a vector to store the heap in the form of array.
+    vector <int> sorted;
     int size,el; // size for the element size of heap and el for taking each element as input
     cout<<"Enter the size of the heap: ";
     cin>>size;
     cout<<"Enter the elements"<<endl;
     for(int i=0;i<size;i++){
         cin>>el;
-        insert(arr,size,el);
+        insert(arr,el);
     }
     cout<<"Printing the heap"<<endl;
     for(auto x: arr)
         cout<<x<<" ";
+    cout<<endl;
+    
+    // deletion(arr,sorted);
+    // for(auto x: arr)
+    //     cout<<x<<" ";
+    heapSort(arr,sorted);
+    cout<<"Printing the heap sort"<<endl;
+    for(auto x:sorted)
+        cout<<x<<" ";
+    cout<<endl;
 }
