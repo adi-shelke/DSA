af2a2d108f615512e92025ae0bbae3a582b0cbed
Adinath Shelke <shelkeadinath3@gmail.com>
2023-04-26 11:04:13 +0530

    HackerRank Level order traversal

---
+++ HackerRank_Level_OrderTraversal.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>	
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+*/
+
+    void levelOrder(Node * root) {
+        queue<Node *> Q;
+        if(root!=NULL){    
+        Q.push(root);
+        while(!Q.empty()){
+            Node* node=Q.front();
+            cout<<node->data<<" ";
+            if(node->left)
+                Q.push(node->left);
+            if(node->right)
+                Q.push(node->right);
+            Q.pop();
+        }
+        }
+
+    }
+
+}; //End of Solution
