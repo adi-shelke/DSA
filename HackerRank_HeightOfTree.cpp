78707944c2d0090e1867470bc3722093e0ee01ec
Adinath Shelke <shelkeadinath3@gmail.com>
2023-04-26 11:03:21 +0530

    Hacker rank height of tree program

---
+++ HackerRank_HeightOfTree.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+        int h = 0;
+        if(root==NULL)
+            return -1;
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+        int maxHeight = max(leftHeight,rightHeight);
+        h = maxHeight+1;
+        return h;
+    }
+
+}; //End of Solution
