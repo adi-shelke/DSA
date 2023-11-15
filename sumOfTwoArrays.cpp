9650fdc89fb5ce342f4ea92357713ee4ef21187c
Adinath Shelke <shelkeadinath3@gmail.com>
2023-11-15 13:18:08 +0530

    Sum  of two arrays coding ninjas

---
+++ sumOfTwoArrays.cpp
@@ -0,0 +1,38 @@
+vector<int> findArraySum(vector<int>&a, int n,vector<int>&b, int m){
+    int i=n-1;
+    int j=m-1;
+    int carry =0;
+    vector<int> ans;
+    while(i>=0 && j>=0){
+        int val1 = a[i];
+        int val2 = b[j];
+        int sum = val1+val2+carry;
+        carry = sum/10;
+        sum = sum%10;
+        ans.push_back(sum);
+        i--;
+        j--;
+    }
+    while(i>=0){
+        int sum= a[i]+carry;
+        carry = sum/10;
+        sum =sum%10;
+        ans.push_back(sum);
+        i--;
+    }
+    while(j>=0){
+        int sum= b[j]+carry;
+        carry = sum/10;
+        sum =sum%10;
+        ans.push_back(sum);
+        j--;
+    }
+    while(carry!=0){
+        int sum =carry;
+        carry =sum/10;
+        sum=sum%10;
+        ans.push_back(sum);
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
