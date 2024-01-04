c4bbfedbb61e34defa153c2b31ba2dfc609ad54a
Adinath Shelke <shelkeadinath3@gmail.com>
2024-01-04 16:08:23 +0530

    Unique Binary Search Trees

---
+++ catalan.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int catalan (int n){
+    int res = 0;
+    if(n<=1)
+        return 1;
+    for(int i=0;i<n;i++)
+        res+=catalan(i)*catalan(n-i-1);
+
+    return res;
+}
+int main(){
+    int n;
+    cin>>n;
+
+        cout<<catalan(n)<<" ";
+
+    return 0;
+}
