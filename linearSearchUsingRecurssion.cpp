3dd96fe6675d61b390182cc1fb1b8c32459467cb
Adinath Shelke <shelkeadinath3@gmail.com>
2024-02-05 16:15:52 +0530

    linear search using recurssion

---
+++ linearSearchUsingRecurssion.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+bool search(int *arr, int size, int target)
+{
+    if (size == 0)
+        return false;
+    if (arr[0] == target)
+        return true;
+    else
+        return search(arr + 1, size - 1, target);
+}
+
+int main()
+{
+
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int target = 6;
+    cout << search(arr, 10, target);
+    return 0;
+}
