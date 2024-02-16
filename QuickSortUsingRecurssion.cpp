a5f5f78dce887e34bbd2660df5fa3c7982477b83
Adinath Shelke <shelkeadinath3@gmail.com>
2024-02-16 23:42:50 +0530

    resolved issues when the values are negative

--- QuickSortUsingRecurssion.cpp
+++ QuickSortUsingRecurssion.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 int partition(vector<int> &arr, int start, int end)
 {
-   int pivot = arr[start];
-   int count  = 0;
-   for(int i=start+1;i<=end;i++)
-       if(arr[i]<pivot)
-           count++;
-    int pivotIndex = start+count;
-    swap(arr[start],arr[pivotIndex]);
-    int i=start,j=end;
-    while(i<pivotIndex && j>pivotIndex)
+    int pivot = arr[start];
+    int count = 0;
+    for (int i = start + 1; i <= end; i++)
+        if (arr[i] <= pivot)
+            count++;
+    int pivotIndex = start + count;
+    swap(arr[start], arr[pivotIndex]);
+    int i = start, j = end;
+    while (i < pivotIndex && j > pivotIndex)
     {
-        while(arr[i]<pivot)
+        while (arr[i] <= pivot)
             i++;
-        while (arr[j]>pivot)
+        while (arr[j] > pivot)
             j--;
-        if(i<pivotIndex && j>pivotIndex)
-            swap(arr[i],arr[j]);
+        if (i < pivotIndex && j > pivotIndex)
+            swap(arr[i], arr[j]);
     }
     return pivotIndex;
 }
@@ -34,8 +34,8 @@ void quickSort(vector<int> &arr, int start, int end)
 int main()
 {
 
-    vector<int> arr = {1, 7, 2, 6, 4, 9, 0};
+    vector<int> arr = {9, 9 ,9 ,8, 2, 3, -6};
     quickSort(arr, 0, arr.size() - 1);
-    for(auto i:arr)
-        cout<<i<<" ";
+    for (auto i : arr)
+        cout << i << " ";
 }
